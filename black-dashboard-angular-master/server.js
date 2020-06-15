41af8afdb9d0e4d6619ada017687a15a2bdb9e4f
jcrossignolli@yahoo.com.br <jcrossignolli@yahoo.com.br>
2020-06-15 13:06:51 -0300

    teste

--- black-dashboard-angular-master/server.js
+++ black-dashboard-angular-master/server.js
@@ -1,6 +1,16 @@
-var http = require('http');
-var app = require('./config/express')();
-const PORT = process.env.PORT || 5000
-http.createServer(app).listen(PORT), function(){
-    console.log('Express Server escutando na porta ' + app.get('port'));
-};
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
